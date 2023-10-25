b989c74ad1d7c0ff2330e08649f33a5d6589972e
Sko0owi <dawid.skowronek24@gmail.com>
2023-10-25 09:17:05 +0200

    ai

--- AI_for_Games/lista1/zad2v2.cpp
+++ AI_for_Games/lista1/zad2v2.cpp
@@ -1,8 +1,8 @@
-// Assignment [3] [Dawid] [Skowronek] [330235] [Sko0owi]
+// Assignment [2] [Dawid] [Skowronek] [330235] [Sko0owi]
 
-#pragma GCC optimize("O3","unroll-loops","inline") //Optimization flags
-//#pragma GCC option("","tune=native","no-zero-upper") //Enable AVX
-#pragma GCC target("arch=haswell,avx")  //Enable AVX
+#pragma GCC optimize("O3","unroll-loops","omit-frame-pointer","inline") //Optimization flags
+#pragma GCC option("arch=native","tune=native","no-zero-upper") //Enable AVX
+#pragma GCC target("avx")  //Enable AVX
 
 #include<bits/stdc++.h>
 
@@ -23,15 +23,15 @@ int KH = 0;
 
 int MAX_DEPTH;
 
-int DEPTH_TO_START = 3;
+int DEPTH_TO_START = 1;
 
 int END_COST = 1000000;
 
-int TIME_FOR_TURN = 990;
+int TIME_FOR_TURN = 145;
 
 int player_color;
 
-pair<int,int> killers[2][500];
+pair<int,int> killers[2];
 
 auto start_time = chrono::high_resolution_clock::now();
 
@@ -52,11 +52,7 @@ static short shiftValL[8] = {
     0,0,0,0,1,9,8,7
 };
 
-bool goodMove(pair<int,int> move)
-{
-    if(move.first != -1 && move.second != -1) return true;
-    return false; 
-}
+
 
 struct gameState
 {
@@ -111,7 +107,6 @@ struct gameState
     {
         enemySpace = 0xAA55AA55AA55AA55;
         mySpace =    0x55AA55AA55AA55AA;
-        color = 0;
     }
 
     ull Shift(ull toShift, int dir)
@@ -119,9 +114,16 @@ struct gameState
         return ((toShift >> shiftValR[dir]) << shiftValL[dir]) & masks[dir];
     }
 
+    // bool goodMove(pair<int,int> move)
+    // {
+    //     if(Get(move.first, mySpace) && Get(move.second,enemySpace)) return true;
+    //     return false;
+    // }
 
     int genPossibleMoves(int depth)
     {
+        ull freeSpace = ~(enemySpace | mySpace);
+
         ull possibleSpace = 0;
 
         possibleSpace |= Shift(mySpace,0) & enemySpace;
@@ -131,6 +133,14 @@ struct gameState
 
 
         int id = 0;
+
+        // if(KH)
+        // {
+
+        //     possibleMoves[depth][id++] = killers[0];
+        //     possibleMoves[depth][id++] = killers[1];
+        
+        // }
 
         for(int i = 0; i < 64; i++)
         {
@@ -147,6 +157,8 @@ struct gameState
                     possibleMoves[depth][id++] = {i+8,i};
             }
         }
+
+        
 
         return id;
     }
@@ -169,39 +181,9 @@ struct gameState
         enemyMobile |= Shift(mySpace,6) & enemySpace;
 
 
-        int mobilePionki = __builtin_popcountll(myMobile) - __builtin_popcountll(enemyMobile);
+        // cerr << myMobile <<  " " << enemyMobile << "\n" << mySpace << " " << enemySpace << "\n";
 
-        int wygrane = 0;
-
-        for(int i = 0; i < 64; i++)
-        {
-
-            ull def = 0, atk = 0;
-
-            def |= Shift((1ull << i), 0) & mySpace;
-            def |= Shift((1ull << i), 0) & mySpace;
-            def |= Shift((1ull << i), 0) & mySpace;
-            def |= Shift((1ull << i), 0) & mySpace;
-
-            atk |= Shift((1ull << i), 0) & enemySpace;
-            atk |= Shift((1ull << i), 0) & enemySpace;
-            atk |= Shift((1ull << i), 0) & enemySpace;
-            atk |= Shift((1ull << i), 0) & enemySpace;
-            
-            if(Get(i,enemySpace))
-            {
-
-                wygrane += (__builtin_popcountll(atk) - __builtin_popcountll(def) >= 0) ? 1 : 0;
-            }
-            if(Get(i,mySpace))
-            {
-                wygrane += (__builtin_popcountll(def) - __builtin_popcountll(atk) >= 0) ? 1 : 0;
-            }
-        }
-
-        wygrane *= -1;
-
-        int ans = mobilePionki * 50 + wygrane * 100;
+        int ans = __builtin_popcountll(myMobile) - __builtin_popcountll(enemyMobile);
         if (player_color != color) ans *= -1;
         return ans;
 
@@ -222,12 +204,19 @@ struct gameState
             newState.mySpace &= ~(1ull << (prev));
             newState.Set(next, &(newState.mySpace));
             newState.enemySpace &= ~(1ull << (next));    
+        } else 
+        {
+            newState.gameEnded = 1;
         }
 
         swap(newState.mySpace, newState.enemySpace);
         return newState;
     }
-
+    pair<int,int> randomMove()
+    {
+        int n = genPossibleMoves(0);
+        return possibleMoves[0][rand() % n + 2];
+    }
     int calculateResult()
     {
         
@@ -245,61 +234,24 @@ int alfa_beta(gameState state, int depth, int alfa, int beta)
     if(CG) if(chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - start_time).count() >= TIME_FOR_TURN) return INT_MIN;
 
 
+    if(state.gameEnded)
+    {
+        //cerr << state.calculateResult() * END_COST << "\n";
+        return state.calculateResult() * END_COST;
+    } 
     if(depth >= MAX_DEPTH) return state.evalState();
-    
 
     
     int n = state.genPossibleMoves(depth);
 
     if(n == 0) return state.calculateResult() * END_COST;
-
+    
     int evalForThis = (depth%2 == 0) ? INT_MIN : INT_MAX;
-
-    if (KH)
-    {
-        for(int i = 0 ; i < 2 ; i++)
-        {
-            if(!goodMove(killers[i][depth]))
-            {
-                continue;
-            }
-            gameState newState = state.doMove(killers[i][depth]);
-
-            int eval = alfa_beta(newState, depth+1, alfa, beta);
-
-            if (eval == INT_MIN) return INT_MIN;
-
-            if(depth % 2 == 0)
-            {
-                evalForThis = max(evalForThis, eval);
-                if(evalForThis >= beta)
-                {
-                    if (i == 1)
-                    {
-                        swap(killers[0][depth], killers[1][depth]);
-                    }
-                    return evalForThis;
-                }
-                alfa = max(alfa,evalForThis);
-            } else 
-            {
-                evalForThis = min(evalForThis, eval);
-                if(evalForThis <= alfa)
-                {
-                    if (i == 1)
-                    {
-                        swap(killers[0][depth], killers[1][depth]);
-                    }
-                    return evalForThis;
-                }
-                beta = min(beta,evalForThis);
-            }
-        } 
-    }
 
 
     for(int i = 0; i < n; i++)
     {
+    
         gameState newState = state.doMove(possibleMoves[depth][i]);
 
         int eval = alfa_beta(newState, depth+1, alfa, beta);
@@ -311,10 +263,6 @@ int alfa_beta(gameState state, int depth, int alfa, int beta)
             evalForThis = max(evalForThis, eval);
             if(evalForThis >= beta)
             {
-                if(KH)
-                {
-                    killers[1][depth] = possibleMoves[depth][i];
-                }
                 return evalForThis;
             }
             alfa = max(alfa,evalForThis);
@@ -323,10 +271,6 @@ int alfa_beta(gameState state, int depth, int alfa, int beta)
             evalForThis = min(evalForThis, eval);
             if(evalForThis <= alfa)
             {
-                if(KH)
-                {
-                    killers[1][depth] = possibleMoves[depth][i];
-                }
                 return evalForThis;
             }
             beta = min(beta,evalForThis);
@@ -348,16 +292,18 @@ pair<int,int> minmaxDecision(gameState toEval)
     while(stillTime && MAX_DEPTH <= 64)
     {
         int n = toEval.genPossibleMoves(0);
+        cerr << "wow: " << n << "\n";
         int maks = INT_MIN;
         pair<int,int> bestMove = {-1,-1};
         for(int i = 0; i < n; i++)
         {
-
             gameState newState = toEval.doMove(possibleMoves[0][i]);
 
             pair<int,int> move = possibleMoves[0][i];
 
             int minmaxEval = alfa_beta(newState, 1, INT_MIN, INT_MAX);
+
+            cerr << "Move: " << i << " " << possibleMoves[0][i].first << " " << possibleMoves[0][i].second << " " << minmaxEval << "\n";
 
             if(minmaxEval == INT_MIN) return bestMoveANS;
 
@@ -389,66 +335,102 @@ int main()
     if (color == "w") player_color = 0;
     else player_color = 1;
 
-    for(int i = 0 ; i < 100; i++)
-    {
-        killers[0][i] = {-1,-1};
-        killers[1][i] = {-1,-1};
-    }
 
 
     gameState game; 
     game.reset();
+    game.color = 0;
 
+    // game loop
     while (1) {
-
-
-        for(int i = 0 ; i < 9; i++)
-        {
-            cerr << "KILLERS :OO of : " << i << "\n";
-            cerr << killers[0][i].first << " " << killers[0][i].second << "\n"; 
-            cerr << killers[1][i].first << " " << killers[1][i].second << "\n"; 
-
-        }
-
-
+        
         // ignore
         for (int i = 7; i >= 0; i--) {
             string line; // horizontal row      
             cin >> line; cin.ignore(); 
         }
 
-        string last_action; 
+        string last_action; // last action made by the opponent ("null" if it's the first turn)
         cin >> last_action; cin.ignore();
 
         if (last_action != "null")
         {
             int prev = (last_action[0] - 'a') + (last_action[1] - '0' - 1) * 8;
             int next = (last_action[2] - 'a') + (last_action[3] - '0' - 1) * 8;
+
+            cerr << "Enemy: " << prev << " " <<  next << "\n";
             game = game.doMove({prev, next});
 
         }
 
+        game.print();
 
-
-        // ignore
         int actions_count; // number of legal actions
         cin >> actions_count; cin.ignore();
+
+        int n = game.genPossibleMoves(0);
+
+
+        cerr << n << " " << actions_count << "\n";
+        if (n != actions_count)
+        {
+            cerr <<"DEBIL 2";
+            return 0;
+        }
+
         
+        for (int i = 0 ; i < n; i++)
+        {
+            int prev = possibleMoves[0][i].first;
+            int next = possibleMoves[0][i].second;
+            string move = (char)(prev%8 + 'a') + to_string((prev/8) +1) + (char)(next%8 + 'a') + to_string((next/8) +1);
+            cerr << i << " " << move << "\n";
+
+        }
+
         start_time = chrono::high_resolution_clock::now();
+
         pair<int,int> decision = minmaxDecision(game);
 
+        if(decision == make_pair(-1,-1))
+        {
+            int n = game.genPossibleMoves(0);
+
+            int moveID = rand() % n;
+
+            int prev = possibleMoves[0][moveID].first;
+            int next = possibleMoves[0][moveID].second;
+
+            string move = (char)(prev%8 + 'a') + to_string((prev/8) +1) + (char)(next%8 + 'a') + to_string((next/8) +1);
+
+            cerr << moveID << " " << move << " " << possibleMoves[0][moveID].first << "\n";
+            cout << move << "\n";
+
+            game = game.doMove({prev,next});
+
+            // cout << "random" << endl;
+            cerr << "Debil\n";
+            return 0;
+            continue;
+        }
 
         int prev = decision.first;
         int next = decision.second;
         string move = (char)(prev%8 + 'a') + to_string((prev/8) +1) + (char)(next%8 + 'a') + to_string((next/8) +1);
         
+        cerr << prev << " " << next << "\n";
         game = game.doMove({prev,next});
 
+        game.print();
 
         cout << move << " MSG ";
         cout << chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - start_time).count() << " " << MAX_DEPTH-2 << " " << bestWAGA << endl;
 
 
         TIME_FOR_TURN = 145;
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+        // cout << "random" << endl; // e.g. e2e3 (move piece at e2 to e3)
     }
 }
