b3df7d9cc94dd7dc8a4475a7bd712c7ea4240e5e
skowi <skowi@skowi.malenstwo>
2024-03-25 02:36:43 +0100

    linux

--- sem6/Kurs_Linux/lista5/zad8.sh
+++ sem6/Kurs_Linux/lista5/zad8.sh
@@ -1,5 +1,20 @@
-while true; do
-    if read line; then
-        echo $(date +"%Y-%m-%d %H-%M-%S") $line
-    fi
-done | nc -lU -vv /var/tmp/dsocket
+exec 1>&4
+while read -u 3 BUFFER
+do
+    case $BUFFER in
+        d) date ;;
+        q) break ;;
+        sys) uname -a ;;
+        *) echo 'What?'
+    esac
+done
+
+# jak podejrzeć komunikacje?
+# sudo socat tcp-listen:5050,fork,reuseaddr -
+# tylko na to wpadłem, podajemy mu port na którym nasłuchujemy i jak akurat na niego przyjdzie wiadomość to słuchamy
+
+# stworzenie socketu co odpala dany ten skrypt
+# socat EXEC:"bash zad8.sh",fdin=3,fdout=4 UNIX-LISTEN:/tmp/mystat.socket
+
+# połączenie się
+# socat - UNIX-CONNECT:/tmp/mystat.socket
